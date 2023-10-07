ed67147f4e0773a887ba5c1108aabcd0ef486ed0
yushoyamaguchi <ysh.824@outlook.jp>
2023-10-07 17:50:30 +0900

    syscall

--- kern/trap.c
+++ kern/trap.c
@@ -191,6 +191,13 @@ trap_dispatch(struct Trapframe *tf)
 		page_fault_handler(tf);
 		return;
 	}
+	if (tf->tf_trapno == T_SYSCALL) {
+		tf->tf_regs.reg_eax = syscall(tf->tf_regs.reg_eax,
+			tf->tf_regs.reg_ebx, tf->tf_regs.reg_ecx,
+			tf->tf_regs.reg_edx, tf->tf_regs.reg_esi,
+			tf->tf_regs.reg_edi);
+		return;
+	}
 	if (tf->tf_trapno == T_BRKPT) {
 		monitor(tf);
 		return;
