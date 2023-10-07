91c1241a2e594d7d74ec5ae110a65069c54f2a8a
yushoyamaguchi <ysh.824@outlook.jp>
2023-10-07 22:14:15 +0900

    err message

--- kern/trap.c
+++ kern/trap.c
@@ -270,7 +270,7 @@ page_fault_handler(struct Trapframe *tf)
 	// the page fault happened in user mode.
 
 	// Destroy the environment that caused the fault.
-	cprintf("[%08x] user fault va %08x ip %08x\n",
+	cprintf("[%08x] user page fault va %08x ip %08x\n",
 		curenv->env_id, fault_va, tf->tf_eip);
 	print_trapframe(tf);
 	env_destroy(curenv);
